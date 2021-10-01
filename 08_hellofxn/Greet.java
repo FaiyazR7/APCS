4576bec2c39f1b61063d38aece26847a4bea167d
FaiyazRafee <frafee30@stuy.edu>
2021-09-30 22:17:33 -0400

    hw 08

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,17 @@
+/*Faiyaz Rafee
+APCS
+HW08 -- Refractor Freshie Zero/Printing strings
+2021-09-30
+
+Notes: You need to use javac first to save the changes otherwise the file runs as it was before editing.
+*/
 	public class Greet {
-		public static void main(String[]args){
-			System.out.println("Hello my fellow fellow :D");
+		public static void main (String [] args){
+			greet("Max");
+			greet("May");
+			greet("Salaj");
 		}
+		static void greet(String str){
+			System.out.println("Hi, how was your day " + str + " ?");
+	}
 	}
