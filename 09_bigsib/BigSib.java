c8cf04d7180f9cd9a40db7c8faa03536ddeca125
FaiyazRafee <frafee30@stuy.edu>
2021-10-03 20:07:38 -0400

    09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/*
+Ubuntus: Salaj: George, May: Perry Jr., Faiyaz: Atlas and Pivot
+APCS
+HW 09 -- What are BigSibs Good For? /Using other java files to run methods
+2021-10-03
+
+DISCOVERIES
+
+-If you want to use methods from a different file, you must javac it first.
+
+*/
+public class BigSib{
+	public static void main(String [] args){
+			}
+	public static void greet(String str){
+		System.out.println("Hi, how was your day " + str + " ?");
+} 
+}
+
+
+
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,18 @@
+/*
+Ubuntus: Salaj: George, May: Perry Jr., Faiyaz: Atlas and Pivot
+APCS
+HW 09 -- What are BigSibs Good For? /Using other java files to run methods
+2021-10-03
+
+DISCOVERIES
+
+-If you want to use methods from a different file, you must javac it first.
+
+*/
+public class Greet {
+	public static void main(String[] args){
+		BigSib.greet("Max");
+		BigSib.greet("May");
+		BigSib.greet("Salaj");
+	}
+}
