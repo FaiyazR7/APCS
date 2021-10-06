48d07fb37030a4b6a7587c0ee632db0c28045d2e
FaiyazRafee <frafee30@stuy.edu>
2021-10-06 00:13:35 -0400

    12

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,23 @@
+/*
+Faiyaz Rafee and Perry Huang
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz/Reassining class to other things
+2021-10-5
+
+DISCOS
+
+Reassining classes to other things lets us use the same class in 
+different ways like how we did with richard and greeting.
+
+QCC
+Seems to be very useful as it simplifies the proccess if you wanted
+ to to use the same class in different ways and/or combine them such as richard.greet.
+
+*/
+public class BigSib{
+	public static void setHelloMsg(String str) {
+		System.out.print(str + " ");
+	}
+	public static String greet(String str) {
+		return str;
+	}}
