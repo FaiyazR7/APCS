ae1b5882aabe95af112d91d818cb783a048a2e3e
FaiyazRafee <frafee30@stuy.edu>
2021-10-06 22:23:59 -0400

    13

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,19 @@
+/*
+Faiyaz Rafee and Perry Huang
+APCS
+HW13 -- On Elder Individuality and the Elimination of Radio Fuzz/Reassining class to other things
+2021-10-6
+DISCOS
+
+QCC
+*/
+public class BigSib{
+	public BigSib() {
+	System.out.print("Word up ");
+	}
+	public static void setHelloMsg(String str) {
+		System.out.print(str + " ");
+	}
+	public static String greet(String str) {
+		return str;
+	}}
