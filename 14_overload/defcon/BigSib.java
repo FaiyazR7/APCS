aa33ce8ce92e2336896b5e465671221c1093da63
FaiyazRafee <78169626+FaiyazR7@users.noreply.github.com>
2021-10-07 23:02:55 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -1,3 +1,17 @@
+/*
+Purple Turtle- Faiyaz Rafee (Pivot and Atlas), Yuki Feng (Paul), Ivina Wang (Sealie) 
+APCS
+HW14 -- Customize your creation/Overloaded and default constructors
+2021/10/7
+
+DISCO:
+Static cannot be used as that would set the constructors the same in all 4 instances of BigSib.
+Overloaded constructors have to come after the default constructor if using multiple constructors.
+
+QCC:
+How do you use one constructor for all 4 instances of BigSib?
+Why does where the overloaded constructor is in the code matter if Java is a compiled language?
+*/
 public class BigSib{
   private String helloMsg;
 
