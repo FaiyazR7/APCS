c816f201e605f24b5cd7c41490f1bd1e03f1e0a5
FaiyazRafee <frafee30@stuy.edu>
2021-10-28 13:18:19 -0400

    27

---
+++ fence.java
@@ -0,0 +1,14 @@
+public class fence {
+  public static String fenceF(int posts){
+    String fence = "|";
+    
+    for(int i = 1; i < post; i++){
+      fence = fence + "--|";
+    }
+    return fence;
+  }
+}
+  public static void main( String [] args) {
+    System.out.print(fenceF(5));
+
+  }
